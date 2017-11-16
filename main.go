c10940d798cada0b4c5b903e8bfbb42b0fa81de5
Tomaž Marhat <tomaz@marhat.com>
2017-11-16 22:00:03 +0100

    chore: updated the client-side postrender to exclude itself from output

--- main.go
+++ main.go
@@ -9,18 +9,76 @@ import (
     "net/http"
     "time"
     "github.com/gorilla/mux"
+    "encoding/json"
+    "sync"
+    "strings"
+    "path/filepath"
 )
 
 type Config struct {
     Delay         int64
     To            string
     Authorization string
+    Output        string
+    sync.Mutex
+}
+
+type Data struct {
+    Content  string `json:"content"`
+    Path     string `json:"path"`
+    Fullpath string `json:"fullpath"`
+}
+
+var c = Config{}
+
+// os.MkDirAll Not working?
+func MkdirR(path []string) {
+
+    for k, _ := range (path) {
+        p, _ := filepath.Abs(filepath.Join(path[:k+1]...))
+        log.Println("Creating: ", p)
+        os.Mkdir(p, 0770)
+    }
+
+}
+
+func WriteData(d *Data) {
+    c.Lock()
+    defer c.Unlock()
+
+    // Insecure, fix
+    path := strings.Replace(d.Fullpath, "..", "", -1)
+
+    paths := strings.Split(path, "/")
+    paths = append([]string{c.Output}, paths...)
+
+    MkdirR(paths)
+
+    outfile := filepath.Join(append(paths, "postrender")...)
+    log.Println("File out: ", outfile)
+
+    err := ioutil.WriteFile(outfile, []byte(d.Content), 0660)
+    if err != nil {
+        log.Println(err)
+    }
+
 }
 
 func handlePost(res http.ResponseWriter, req *http.Request) {
     res.Header().Set("Access-Control-Allow-Origin", "*")
+    defer req.Body.Close()
+
+    decoder := json.NewDecoder(req.Body)
+    d := Data{}
+    err := decoder.Decode(&d)
+
+    if err != nil {
+        res.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    go WriteData(&d)
     res.WriteHeader(http.StatusOK)
-    log.Println(req)
     return
 }
 
@@ -30,6 +88,7 @@ func main() {
     to := flag.String("host", "", "Url to which a POST request is made with the HTML, eg. http://127.0.20:1337/renderer")
     auth := flag.String("auth", "", "Auth string for Authorization header")
     listen := flag.String("listen", "", "Port on which to listen for incomming requests, ie: :1337")
+    target := flag.String("dir", "", "Directory in which to save the rendered content")
 
     flag.Parse()
     if len(*to) <= 0 {
@@ -37,7 +96,24 @@ func main() {
         os.Exit(1)
     }
 
-    c := Config{*delay, *to, *auth}
+    s, err := os.Stat(*target)
+    if err != nil || !s.IsDir() {
+        if err != os.ErrNotExist {
+            log.Println(err)
+            os.Exit(2)
+        }
+    }
+
+    log.Printf("Outputting to: %s\n", *target)
+
+    // No need for mutex yet, but maybe we'll provide a live config change
+    // And where's the fun without mutexes? Mutices? Those lock things.
+    c.Lock()
+    c.Delay = *delay
+    c.Output = *target
+    c.To = *to
+    c.Authorization = *auth
+    c.Unlock()
 
     tcontent, err := ioutil.ReadFile("template.txt")
     if err != nil {
